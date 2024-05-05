85fe91d86fb2b9d2cfe13163115a0edcd53e2925
Andreas Tsatsanis <andtsa@ambtwo.home>
2024-05-05 16:07:30 +0200

    implement (correctly) the threaded local runner + 1 integration test for it

--- src/local/mod.rs
+++ src/local/mod.rs
@@ -1,2 +1,5 @@
+<<<<<<< HEAD
 /// the (first iteration) thread pool implementation
+=======
+>>>>>>> d1aa084 (implement (correctly) the threaded local runner + 1 integration test for it)
 pub mod runner;
--- src/local/runner.rs
+++ src/local/runner.rs
@@ -9,9 +9,14 @@ use crate::error::GourdError;
 
 /// # Multithreaded _local_ runner for tasks
 /// (more documentation needed tbh)
+<<<<<<< HEAD
 #[allow(dead_code, unused)]
 pub fn run_locally(tasks: Vec<Command>) -> Result<Vec<ExitStatus>, GourdError> {
     let rt = runtime::Runtime::new().map_err(GourdError::IoError)?;
+=======
+pub fn run_locally(tasks: Vec<Command>) -> Result<Vec<ExitStatus>, GourdError> {
+    let rt = runtime::Runtime::new().unwrap();
+>>>>>>> d1aa084 (implement (correctly) the threaded local runner + 1 integration test for it)
 
     rt.block_on(async {
         let task_futures: Vec<_> = tasks
@@ -26,10 +31,18 @@ pub fn run_locally(tasks: Vec<Command>) -> Result<Vec<ExitStatus>, GourdError> {
                 })
             })
             .collect();
+<<<<<<< HEAD
         // Run all commands concurrently and collect their results
         let results = join_all(task_futures).await;
         let mut output = vec![];
         for result in results.into_iter() {
+=======
+        // .collect::<Vec<dyn Future<Output=ExitStatus>>>();
+        // Run all commands concurrently and collect their results
+        let results = join_all(task_futures).await;
+        let mut output = vec![];
+        for (_i, result) in results.into_iter().enumerate() {
+>>>>>>> d1aa084 (implement (correctly) the threaded local runner + 1 integration test for it)
             match result {
                 Ok(status) => output.push(status),
                 Err(err) => return Err(GourdError::ChildJoinError(err)),
