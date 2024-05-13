8147ba95c08f891783e4d3374a043971a1eab25e
Mikołaj Gazeel <m.j.gazeel@student.tudelft.nl>
2024-05-13 17:05:26 +0200

    latex2man pipeline

--- docs/user/compile.sh
+++ docs/user/compile.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 mkdir out
 
 compile_xelatex() {
     xelatex -halt-on-error -shell-escape -interaction=nonstopmode -output-directory=./out gourd.tex
 }
 
-compile_xelatex && compile_xelatex
+compile_xelatex && compile_xelatex && mv ./out/gourd.pdf ./../../gourd-user-documentation.pdf
+latex2man gourd.tex ./../../gourd.man
